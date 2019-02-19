4e1eebc90cfa25ce8a39ddbaeb922845e305c931
Ligeest <30897514+Ligeest@users.noreply.github.com>
2019-02-19 14:39:20 +0800

    Add files via upload

---
+++ 7.js
@@ -0,0 +1,13 @@
+// JavaScript Document
+function fun(){
+	alert("sb");
+}
+//fun();
+
+function sum(qian,hou){
+	//console.log(qian+hou);
+	return(qian+hou);
+}
+var result;
+result = sum(33,2);
+alert(result);
